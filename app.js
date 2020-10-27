4eb51b011e2aaffb1159ca16561904f4bc370372
oxn <csot.rpt03@csot.TCL.com>
2020-10-27 09:30:51 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+const request = require('request')
+const colors = require('colors')
+const iconv = require('iconv-lite');
+const exec = require('child_process').exec
+const http = require('http');
+const config = require('./config');
+const BufferHelper = require('bufferhelper');
+
+const space = {
+    s2: "  ",
+    s4: "    ",
+    s8: "        ",
+    s16: "                "
+};
+
+const stockIdList = config.stocks;
+
+let stockResultList = [];
+
+function renderList() {
+    console.log('股票名称' +
+        space.s4 +
+        '昨日收盘' +
+        space.s4 +
+        '当前价格' +
+        space.s4 +
+        '今日涨幅' +
+        space.s4 +
+        '今日最高' +
+        space.s4 +
+        '今日最低');
+    for (let i = 0; i < stockResultList.length; i++) {
+        const item = stockResultList[i];
+        let rate = 0.00;
+        let rateText = '0.00%';
+        if (item[3] != 0) {
+            let dif = item[3] - item[2];
+            if (dif >= 0) {
+                rate = (dif / item[2] * 100).toFixed(2);
+                rateText = colors.red(rate + '%');
+            } else {
+                rate = (dif / item[2] * 100).toFixed(2);
+                rateText = colors.green(rate + '%');
+            }
+        }
+        console.log("");
+        console.log(colors.red(item[0]) +
+            space.s4 +
+            colors.yellow(item[2]) +
+            space.s8 +
+            colors.red(item[3]) +
+            space.s8 +
+            colors.red(rateText) +
+            space.s8 +
+            colors.red(item[4]) +
+            space.s8 +
+            colors.red(item[5])
+        );
+    }
+}
+
+function getData() {
+    let process = 0;
+    stockResultList = [];
+    clear();
+    for (let i = stockIdList.length - 1; i >= 0; i--) {
+        const item = stockIdList[i];
+        let urlStr = config.api;
+        urlStr = urlStr + item;
+        const url = require('url').parse(urlStr);
+        if(config.proxy.used){
+            url.host = config.proxy.host;
+            url.port = config.proxy.port;
+            url.headers = {
+                Host: config.proxy.headerHost
+            }
+        }
+        http.get(url, function (res) {
+            const bufferHelper = new BufferHelper();
+            res.on('data', function (chunk) {
+                bufferHelper.concat(chunk);
+            });
+            res.on('end', function () {
+                //console.log();
+                process = process + 1;
+                let str = iconv.decode(bufferHelper.toBuffer(), 'GBK');
+                str = str.substring(str.indexOf("\"") + 1, str.lastIndexOf("\""));
+                let arr = str.split(',');
+                stockResultList.push(arr);
+                if (process == stockIdList.length) {
+                    renderList();
+                    setTimeout(function () {
+                        getData();
+                    }, 5000);
+                }
+            });
+        })
+    }
+}
+
+function clear() {
+    let stdout = "";
+    stdout += "\033c";
+    stockResultList = [];
+    process.stdout.write(stdout);
+}
+
+getData();
---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+    api:'http://hq.sinajs.cn/list=',
+
+    proxy :{
+        used: false,
+        port: '',
+        host: '',
+        headerHost:''
+    },
+
+    stocks:['sh600050']
+};
+
+module.exports = config;
